ee68e21eca1f577c4e065e896fc260f7557cb4e7
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-25 16:53:53 -0300

    feat: moved handlers to separeted files

--- handler/handler.go
+++ handler/handler.go
@@ -1,37 +1,3 @@
 package handler
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-func OpeningGetAllHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
-}
-
-func OpeningGetOneHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
-}
-
-func OpeningCreateHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening",
-	})
-}
-
-func OpeningUpdateHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "PUT Opening",
-	})
-}
-
-func OpeningDeleteHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "DELETE Opening",
-	})
-}
+func Lala() {}
