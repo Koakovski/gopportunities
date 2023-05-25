8648a60822556fd8617df25ee3248ab2c213b07f
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-25 16:08:48 -0300

    feat: added routes to Opening CRUD

--- router/routes.go
+++ router/routes.go
@@ -10,9 +10,34 @@ func initializeRoutes(router *gin.Engine) {
 	v1RouterGroup := router.Group("/api/v1")
 
 	{
+		// GET ALL OPENINGS
 		v1RouterGroup.GET("/opening", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "Get Opening",
+				"msg": "GET Openings",
+			})
+		})
+		// GET OPENING BY ID
+		v1RouterGroup.GET("/opening/:id", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Opening",
+			})
+		})
+		// CREATE NEW OPENING
+		v1RouterGroup.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "POST Opening",
+			})
+		})
+		// UPDATE OPENING
+		v1RouterGroup.PUT("/opening/:id", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "PUT Opening",
+			})
+		})
+		// DELETE OPENING
+		v1RouterGroup.DELETE("/opening/:id", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "DELETE Opening",
 			})
 		})
 	}
