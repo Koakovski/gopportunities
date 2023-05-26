c47e31031760ddef47b1bc1ef23c71aa3065ae8c
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-26 16:51:02 -0300

    feat: added logic to OpeningGetOneHandler

--- handler/openning_get_one_handler.go
+++ handler/openning_get_one_handler.go
@@ -1,13 +1,30 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Koakovski/gopportunities/schema"
 	"github.com/gin-gonic/gin"
 )
 
 func OpeningGetOneHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
+	openingId := ctx.Param("openingId")
+
+	if openingId == "" {
+		err := errParamIsRequired("openingId", "param")
+		logger.Errorf("error get one opening: %v", err.Error())
+		sendResponseError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schema.Opening{}
+
+	if err := db.First(&opening, openingId).Error; err != nil {
+		logger.Errorf("error get one opening: %v", err.Error())
+		sendResponseError(ctx, http.StatusNotFound, fmt.Errorf("opening with id %s not found", openingId).Error())
+		return
+	}
+
+	sendResponse(ctx, http.StatusOK, opening)
 }
