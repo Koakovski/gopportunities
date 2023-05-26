d6f03cb2f083d1c873151392169b19185b29f964
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-26 16:39:31 -0300

    feat: added logic to OpeningDeleteHandler

--- handler/openning_delete_handler.go
+++ handler/openning_delete_handler.go
@@ -1,13 +1,36 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Koakovski/gopportunities/schema"
 	"github.com/gin-gonic/gin"
 )
 
 func OpeningDeleteHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "DELETE Opening",
-	})
+	openingId := ctx.Param("openingId")
+
+	if openingId == "" {
+		err := errParamIsRequired("openingId", "param")
+		logger.Errorf("error deleting opening: %v", err.Error())
+		sendResponseError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schema.Opening{}
+
+	if err := db.First(&opening, openingId).Error; err != nil {
+		logger.Errorf("error deleting opening: %v", err.Error())
+		sendResponseError(ctx, http.StatusNotFound, fmt.Errorf("opening with id %s not found", openingId).Error())
+		return
+	}
+
+	if err := db.Delete(&opening).Error; err != nil {
+		logger.Errorf("error deleting opening: %v", err.Error())
+		sendResponseError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendResponse(ctx, http.StatusNoContent, nil)
 }
